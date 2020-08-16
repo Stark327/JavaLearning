6aa5ba70bd9f332b38253be530c72db6723488ad
stark1307 <749815933@qq.com>
2020-08-16 23:30:11 +0800

    feat: add author message

--- src/com/stark1307/day03annotationinjection/SpringAnnotationTest.java
+++ src/com/stark1307/day03annotationinjection/SpringAnnotationTest.java
@@ -3,6 +3,9 @@ package com.stark1307.day03annotationinjection;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * @author stark1307
+ */
 public class SpringAnnotationTest {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("com/stark1307/day03annotationinjection/applicationContext.xml");
